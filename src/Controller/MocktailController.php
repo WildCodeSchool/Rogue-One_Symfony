ddd6f3d230f6f2f2325485af183ca1ed49fc2477
chouaibj <janahchouaib@gmail.com>
2024-02-19 17:41:28 +0100

    Update MocktailController.php

--- src/Controller/MocktailController.php
+++ src/Controller/MocktailController.php
@@ -12,7 +12,7 @@ class MocktailController extends AbstractController
     public function index(): Response
     {
         return $this->render('mocktail/index.html.twig', [
-            'pageName' => 'Mocktails',
+            'pageName' => "Nos Mocktails",
         ]);
     }
 }
