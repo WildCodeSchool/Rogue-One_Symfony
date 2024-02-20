c958f6d477bf34d96dd406b0cc82fb56a12db482
Joël MAYEMBA <joelmayemba@gmail.com>
2024-02-20 14:30:00 +0100

    --fix

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('styles', './assets/styles/styles.css')
+    .addStyleEntry('footer', './assets/styles/components/footer.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
