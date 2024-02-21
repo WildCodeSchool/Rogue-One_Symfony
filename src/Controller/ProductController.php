01a15d0a9707852c2bf45517db9a97fc1b172a41
chouaibj <janahchouaib@gmail.com>
2024-02-21 21:18:18 +0100

    Update rename controler and index and add show controller and route

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/product', name: 'app_product_')]
+class ProductController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(Request $request): Response
+    {
+        $categories = $this->entityManager->getRepository(Category::class)->findAll();
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $productsPerPage = 10;
+        $totalproducts = count($products);
+        $pagesNumber = ceil($totalproducts / $productsPerPage);
+
+        $page = $request->query->getInt('page', 1);
+
+        return $this->render('product/index.html.twig', [
+            'pageName' => "Nos Mocktails",
+            'categories' => $categories,
+            'products' => $products,
+            'pagesNumber' => $pagesNumber,
+            'currentPage' => $page,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    public function show(int $id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy(['id' => $id]);
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+        return $this->render(
+            'product/show.html.twig',
+            ['product' => $product]
+        );
+    }
+}
