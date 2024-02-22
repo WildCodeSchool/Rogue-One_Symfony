fab4c02e0a4b5069dc2198f19c4ad662cd52510b
Joël MAYEMBA <joelmayemba@gmail.com>
2024-02-22 14:09:09 +0100

    Update MemberFixtures.php

--- src/DataFixtures/MemberFixtures.php
+++ src/DataFixtures/MemberFixtures.php
@@ -10,24 +10,35 @@ class MemberFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-    
-        $member1 = new Member();
-        $member1->setFirstname('Chouiab');
-        $member1->setLastname('JANAH');
-        $member1->setRole('Product Owner');
-        $member1->setImg('../assets/images/team/member1.png');
-        $member1->setLink('https://www.linkedin.com/in/cjanah/');
-        $member1->setDescription("");
-        $manager->persist($member1);
+        $membersData = [
+            [
+                'firstname' => 'Chouiab',
+                'lastname' => 'JANAH',
+                'role' => 'Product Owner',
+                'img' => '../assets/images/team/member1.png',
+                'link' => 'https://www.linkedin.com/in/cjanah/',
+                'description' => "Description du Product Owner",
+            ],
+            [
+                'firstname' => 'Joël',
+                'lastname' => 'MAYEMBA',
+                'role' => 'Scrum Master',
+                'img' => '../assets/images/team/member3.png',
+                'link' => 'https://www.linkedin.com/in/mayemba/',
+                'description' => "Description du Scrum Master",
+            ],
+        ];
 
-        $member2 = new Member();
-        $member2->setFirstname('Joël');
-        $member2->setLastname('MAYEMBA');
-        $member2->setRole('Scrum Master');
-        $member2->setImg('../assets/images/team/member3.png');
-        $member2->setLink('https://www.linkedin.com/in/mayemba/');
-        $member2->setDescription("");
-        $manager->persist($member2);
+        foreach ($membersData as $memberData) {
+            $member = new Member();
+            $member->setFirstname($memberData['firstname']);
+            $member->setLastname($memberData['lastname']);
+            $member->setRole($memberData['role']);
+            $member->setImg($memberData['img']);
+            $member->setLink($memberData['link']);
+            $member->setDescription($memberData['description']);
+            $manager->persist($member);
+        }
 
         $manager->flush();
     }
