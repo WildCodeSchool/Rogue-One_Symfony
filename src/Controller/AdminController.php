7c17d8b0159e86c0755705a3797b24ac76d6d0c8
Joël MAYEMBA <joelmayemba@gmail.com>
2024-02-26 00:32:37 +0100

    Add Admin
    Beware huge for the admin dashboard. To delete later

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $tableName = Product::class;
+        $products = $productRepository->findAll();
         return $this->render(
-            'admin/index.html.twig',
-            ['pageName' => "Page d'admin"]
+            'admin/admin.html.twig',
+            [
+                'pageName' => 'Page Administrateur',
+                'tableName' => $tableName,
+                'products' => $products,
+            ]
         );
     }
 }
