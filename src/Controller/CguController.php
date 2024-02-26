70cf36bd96884c71d56e86b68c0b98587c609731
Joël MAYEMBA <joelmayemba@gmail.com>
2024-02-26 11:04:27 +0100

    update

--- src/Controller/CguController.php
+++ src/Controller/CguController.php
@@ -12,7 +12,6 @@ class CguController extends AbstractController
     public function index(): Response
     {
         return $this->render('cgu/index.html.twig', [
-            'controller_name' => 'CguController',
             'pageDescription' => "CGU",
             'pageName' => "CGU",
             'banner' => "cgu",
